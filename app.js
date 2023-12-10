41b6a8aa9f912e4bd7d715726f70745bef8d36da
Rodrigo Sturm <rodristurm@gmail.com>
2023-12-10 00:05:50 -0300

    add mqtt module and functions

--- app.js
+++ app.js
@@ -13,3 +13,35 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`Running at port ${PORT}`);
 });
+
+const mqtt = require('mqtt');
+const clientId = 'emqx_nodejs_' + Math.random().toString(16).substring(2, 8);
+const username = 'Rodri';
+const password = 'NodeJS';
+
+const client = mqtt.connect('mqtt://broker.emqx.io:1883', {
+    clientId,
+    username,
+    password,
+});
+
+const topic = 'posicion/lugar';
+const payload = 'conectado';
+const qos = 0;
+
+client.subscribe(topic, { qos }, (error) => {
+    if (error){
+        console.log('Subscribe error...');
+    }
+    console.log(`Suscripcion exitosa al topico ${topic}`);
+});
+
+client.publish(topic, payload, { qos }, (error) => {
+    if(error){
+        console.log('Publish error...');
+    }
+});
+
+client.on('message', (topic, payload) => {
+    console.log('Mnesaje recibido: ', topic, payload.toString());
+});
