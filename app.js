61adf79feedc2b41c46f9568dbacd59078075668
Rodrigo Sturm <rodristurm@gmail.com>
2023-12-17 15:55:07 -0300

    changed the content type

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ fs.readFile('./index.html', function (err, html) {
     if (err) throw err;
 
     http.createServer(function(req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(html);
         res.end();
     }).listen(PORT, () => {
